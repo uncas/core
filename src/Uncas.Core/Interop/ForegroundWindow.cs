ca158d965c9e7516013e3e50ffb59d17ed6ded56
Ole Lynge Soerensen <olelynge@gmail.com>
2011-06-15 09:43:02 +0000

    Adding interop classes.
    git-svn-id: http://localhost:81/svn/dev/modules/Uncas.Core/trunk@1129 90d2b0b2-0e11-ba47-89f1-85e69862abc4

---
+++ src/Uncas.Core/Interop/ForegroundWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Uncas.Core.Interop
+{
+    public class ForegroundWindow
+    {
+        // Declare external functions.
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(
+            IntPtr hWnd,
+            StringBuilder text,
+            int count);
+
+        public static string GetForegroundWindowTitle()
+        {
+            int chars = 256;
+            StringBuilder buff = new StringBuilder(chars);
+
+            // Obtain the handle of the active window.
+            IntPtr handle = GetForegroundWindow();
+
+            // Update the controls.
+            if (GetWindowText(handle, buff, chars) > 0)
+            {
+                return buff.ToString();
+            }
+
+            return null;
+        }
+    }
+}
