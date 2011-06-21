dc3e5202cace8366bd4bcdb0e65e7c4dfe1dcaa3
Ole Lynge Soerensen <olelynge@gmail.com>
2011-06-21 14:39:41 +0000

    Adding OleDb stuff to core.
    git-svn-id: http://localhost:81/svn/dev/modules/Uncas.Core/trunk@1278 90d2b0b2-0e11-ba47-89f1-85e69862abc4

---
+++ src/Uncas.Core/Data/OleDbContext.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Globalization;
+
+namespace Uncas.Core.Data
+{
+    public class OleDbContext
+    {
+        private readonly string _connectionString;
+
+        public OleDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DataTable GetData(string commandText,
+            params OleDbParameter[] parameters)
+        {
+            DataTable dt = new DataTable("uncas-data");
+            dt.Locale = CultureInfo.InvariantCulture;
+            using (OleDbConnection connection = new OleDbConnection(_connectionString))
+            {
+                using (OleDbCommand command = new OleDbCommand(commandText, connection))
+                {
+                    foreach (OleDbParameter parameter in parameters)
+                        command.Parameters.Add(parameter);
+                    using (OleDbDataAdapter adapter = new OleDbDataAdapter(command))
+                    {
+                        connection.Open();
+                        adapter.Fill(dt);
+                        connection.Close();
+                    }
+                }
+            }
+            return dt;
+        }
+
+        public DbDataReader GetReader(string commandText,
+            params OleDbParameter[] parameters)
+        {
+            OleDbConnection connection = new OleDbConnection(_connectionString);
+            OleDbCommand command = new OleDbCommand(commandText, connection);
+            foreach (OleDbParameter parameter in parameters)
+                command.Parameters.Add(parameter);
+            connection.Open();
+            OleDbDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            return reader;
+        }
+
+        public int ModifyData(string commandText,
+            params OleDbParameter[] parameters)
+        {
+            int iOut = 0;
+            using (OleDbConnection connection = new OleDbConnection(_connectionString))
+            {
+                using (OleDbCommand command = new OleDbCommand(commandText, connection))
+                {
+                    foreach (OleDbParameter parameter in parameters)
+                        command.Parameters.Add(parameter);
+                    connection.Open();
+                    iOut = command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            return iOut;
+        }
+
+        public int? GetInt32(string commandText,
+            params OleDbParameter[] parameters)
+        {
+            int? iOut = null;
+            using (OleDbConnection connection = new OleDbConnection(_connectionString))
+            {
+                using (OleDbCommand command = new OleDbCommand(commandText, connection))
+                {
+                    foreach (OleDbParameter parameter in parameters)
+                        command.Parameters.Add(parameter);
+                    connection.Open();
+                    object o = command.ExecuteScalar();
+                    if (!(o is DBNull))
+                        iOut = (int)o;
+                    connection.Close();
+                }
+            }
+            return iOut;
+        }
+    }
+}
