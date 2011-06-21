4c0d6f882ff0232f61ac95fead5a28901af3c5c0
Ole Lynge Soerensen <olelynge@gmail.com>
2011-06-21 10:36:32 +0000

    Moving common IOC stuff to core.
    git-svn-id: http://localhost:81/svn/dev/modules/Uncas.Core/trunk@1256 90d2b0b2-0e11-ba47-89f1-85e69862abc4

---
+++ src/Uncas.Core/Ioc/BaseBootstrapper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Uncas.TimeWatcher
+{
+    /// <summary>
+    /// Holds common bootstrapper logic.
+    /// </summary>
+    public class BaseBootstrapper
+    {
+        private IIocContainer _container;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseBootstrapper"/> class.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        public BaseBootstrapper(IIocContainer container)
+        {
+            _container = container;
+            RegisterAutomagically();
+        }
+
+        /// <summary>
+        /// Resolves the given type.
+        /// </summary>
+        /// <typeparam name="T">The type to resolve.</typeparam>
+        /// <returns>
+        /// An instance of the given type.
+        /// </returns>
+        public T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        private void RegisterAutomagically()
+        {
+            Assembly entryAssembly = Assembly.GetEntryAssembly();
+            RegisterImplementationsInAssembly(
+                entryAssembly);
+            IEnumerable<AssemblyName> referencedAssemblyNames =
+                entryAssembly.GetReferencedAssemblies()
+                .Where(x => x.Name.StartsWith("Uncas.", StringComparison.OrdinalIgnoreCase));
+            foreach (var referencedAssemblyName in referencedAssemblyNames)
+            {
+                Assembly referencedAssembly =
+                    Assembly.Load(referencedAssemblyName);
+                RegisterImplementationsInAssembly(
+                    referencedAssembly);
+            }
+        }
+
+        private void RegisterImplementationsInAssembly(
+            Assembly assembly)
+        {
+            foreach (Type implementationType in assembly.GetTypes())
+            {
+                Type interfaceType = implementationType
+                    .GetInterfaces()
+                    .SingleOrDefault(
+                        x => x.Name == "I" + implementationType.Name);
+                if (interfaceType == null)
+                {
+                    continue;
+                }
+
+                _container.RegisterType(implementationType, interfaceType);
+            }
+        }
+    }
+}
---
+++ src/Uncas.Core/Ioc/IIocContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uncas.TimeWatcher
+{
+    /// <summary>
+    /// Represents an inversion-of-control container.
+    /// </summary>
+    public interface IIocContainer
+    {
+        /// <summary>
+        /// Resolves the type T.
+        /// </summary>
+        /// <typeparam name="T">The type to resolve.</typeparam>
+        /// <returns>An instance of type T.</returns>
+        T Resolve<T>();
+
+        /// <summary>
+        /// Registers the type.
+        /// </summary>
+        /// <param name="implementationType">Type of the implementation.</param>
+        /// <param name="interfaceType">Type of the interface.</param>
+        void RegisterType(Type implementationType, Type interfaceType);
+    }
+}
