992378e5b044430d6d63511577aaf6ff47331d7f
Ole Lynge Soerensen <olelynge@gmail.com>
2011-06-21 10:53:49 +0000

    Added suppression of name space with too few types.
    git-svn-id: http://localhost:81/svn/dev/modules/Uncas.Core/trunk@1260 90d2b0b2-0e11-ba47-89f1-85e69862abc4

---
+++ src/Uncas.Core/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[module: SuppressMessage(
+    "Microsoft.Design",
+    "CA1020:AvoidNamespacesWithFewTypes",
+    Scope = "namespace",
+    Target = "Uncas.Core",
+    Justification = "The Uncas.Core namespace holds infrastructure code that is not application specific; with time it is expected to contain a range of types.")]
