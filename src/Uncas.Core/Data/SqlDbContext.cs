40273547da36ca7800e018d66d37e8b21f6a2f99
Ole Lynge Soerensen <olelynge@gmail.com>
2011-07-02 07:24:25 +0000

    Adding SqlDbContext.
    git-svn-id: http://localhost:81/svn/dev/modules/Uncas.Core/trunk@1329 90d2b0b2-0e11-ba47-89f1-85e69862abc4

---
+++ src/Uncas.Core/Data/SqlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace Uncas.Core.Data
+{
+    /// <summary>
+    /// SQL db context.
+    /// </summary>
+    public class SqlDbContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SqlDbContext"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        public SqlDbContext(string connectionString)
+            : base(SqlClientFactory.Instance, connectionString)
+        {
+        }
+    }
+}
