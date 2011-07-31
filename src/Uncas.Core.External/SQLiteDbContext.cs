56712f7ea209064d3fb458b661d40b8bbeb24fba
Ole Lynge Soerensen <olelynge@gmail.com>
2011-07-31 08:11:06 +0000

    Moving SQLite infrastructure stuff to Uncas.Core.External.
    git-svn-id: http://localhost:81/svn/dev/modules/Uncas.Core/trunk@1391 90d2b0b2-0e11-ba47-89f1-85e69862abc4

---
+++ src/Uncas.Core.External/SQLiteDbContext.cs
@@ -0,0 +1,26 @@
+﻿namespace Uncas.Core.External
+{
+    using System.Data.SQLite;
+    using System.Diagnostics.CodeAnalysis;
+    using Uncas.Core.Data;
+
+    /// <summary>
+    /// Db context for SQLite.
+    /// </summary>
+    [SuppressMessage(
+        "Microsoft.Naming",
+        "CA1709:IdentifiersShouldBeCasedCorrectly",
+        MessageId = "Db",
+        Justification = "Follows how 'Db' is cased in .NET framework library System.Data.")]
+    public abstract class SQLiteDbContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SQLiteDbContext"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        protected SQLiteDbContext(string connectionString)
+            : base(SQLiteFactory.Instance, connectionString)
+        {
+        }
+    }
+}
