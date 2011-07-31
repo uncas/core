b23fe526ebe4ec6ead310af722563aa16a9bd129
Ole Lynge Soerensen <olelynge@gmail.com>
2011-07-31 20:10:36 +0000

    Created a red test that exhibits what needs to be done...
    git-svn-id: http://localhost:81/svn/dev/modules/Uncas.Core/trunk@1410 90d2b0b2-0e11-ba47-89f1-85e69862abc4

--- tests/Uncas.Core.Tests/Logging/LogEntryTests.cs
+++ tests/Uncas.Core.Tests/Logging/LogEntryTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Uncas.Core.Tests.Logging
 {
+    using System;
     using System.IO;
     using System.Web;
     using NUnit.Framework;
@@ -8,11 +9,15 @@
     [TestFixture]
     public class LogEntryTests
     {
+        [SetUp]
+        public void BeforeEach()
+        {
+            HttpContext.Current = null;
+        }
+
         [Test]
         public void LogEntry_WithoutHttpContext_WithoutHttpState()
         {
-            HttpContext.Current = null;
-
             var logEntry = new LogEntry(LogType.Error, "test", null, null);
 
             Assert.IsNull(logEntry.HttpState);
@@ -32,7 +37,39 @@
 
             LogEntryHttpState httpState = logEntry.HttpState;
             Assert.IsNotNull(httpState);
-            Assert.AreEqual(url, httpState.Url);
+            Assert.AreEqual(url, httpState.Url.AbsoluteUri);
+        }
+
+        [Test]
+        public void LogEntry_WithException_FileNameFromException()
+        {
+            var exception = new Exception("Test exception.");
+            var logEntry = new LogEntry(LogType.Error, "test", exception, null);
+
+            Assert.That(logEntry.FileName.EndsWith("LogEntryTests.cs"));
+            Assert.AreEqual(47, logEntry.LineNumber);
+        }
+
+        [Test]
+        public void LogEntry_ThrownException_FileNameFromException()
+        {
+            try
+            {
+                throw new Exception("Test exception.");
+            }
+            catch (Exception exception)
+            {
+                var logEntry = new LogEntry(LogType.Error, "test", exception, null);
+                Assert.That(logEntry.FileName.EndsWith("LogEntryTests.cs"));
+            }
+        }
+
+        [Test]
+        public void LogEntry_WithoutException_FileNameFromCaller()
+        {
+            var logEntry = new LogEntry(LogType.Warning, "test", null, null);
+
+            Assert.That(logEntry.FileName.EndsWith("LogEntryTests.cs"));
         }
     }
 }
---
+++ tests/Uncas.Core.Tests/Logging/LoggerTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Uncas.Core.Tests.Logging
+{
+    using System;
+    using Moq;
+    using NUnit.Framework;
+    using Uncas.Core.Logging;
+
+    [TestFixture]
+    public class LoggerTests
+    {
+        private ILogger _logger;
+        private Mock<ILogRepository> _logRepositoryMock;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _logRepositoryMock = new Mock<ILogRepository>();
+            _logger = new Logger(_logRepositoryMock.Object);
+        }
+
+        [Test]
+        public void Log_WithDescription_SavesDescription()
+        {
+            string description = Guid.NewGuid().ToString();
+
+            _logger.Log(LogType.Error, description);
+
+            _logRepositoryMock.Verify(
+                x => x.Save(It.Is<LogEntry>(y => y.Description == description)));
+        }
+
+        [Test]
+        public void Log_WithoutException_FileNameFromCaller()
+        {
+            _logger.Log(LogType.Error, "test");
+
+            _logRepositoryMock.Verify(
+                x => x.Save(It.Is<LogEntry>(y => y.FileName.EndsWith("LoggerTests.cs"))));
+        }
+    }
+}
