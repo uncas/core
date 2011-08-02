b48784100412358cfc1adfcc6e57873a6f9949f8
Ole Lynge Soerensen <olelynge@gmail.com>
2011-08-02 16:23:00 +0000

    Added text handler to core.
    git-svn-id: http://localhost:81/svn/dev/modules/Uncas.Core/trunk@1437 90d2b0b2-0e11-ba47-89f1-85e69862abc4

---
+++ src/Uncas.Core/Web/TextHandler.cs
@@ -0,0 +1,52 @@
+﻿namespace Uncas.Core.Web
+{
+    using System;
+
+    /// <summary>
+    /// Handles text for web.
+    /// </summary>
+    public static class TextHandler
+    {
+        /// <summary>
+        /// Inserts line breaks.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <returns></returns>
+        public static string InsertLineBreaks(string text)
+        {
+            return InsertLineBreaks(text, 1); // The default...
+        }
+
+        /// <summary>
+        /// Inserts line breaks.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="numberOfLineBreaks">The number of line breaks.</param>
+        /// <returns>The text with line breaks.</returns>
+        public static string InsertLineBreaks(
+            string text, 
+            int numberOfLineBreaks)
+        {
+            string outString = string.Empty;
+            if (string.IsNullOrEmpty(text))
+                return outString;
+            numberOfLineBreaks = Math.Max(1, numberOfLineBreaks);
+            foreach (char karakter in text.ToCharArray())
+            {
+                if (char.ConvertToUtf32(karakter.ToString(), 0) == 13)
+                {
+                    for (int i = 0; i < numberOfLineBreaks; i++)
+                    {
+                        outString += "<br/>";
+                    }
+                }
+                else
+                {
+                    outString += karakter.ToString();
+                }
+            }
+ 
+            return outString;
+        }
+    }
+}
