6b3abfc93c7a99fdbb9c40ce5b685b58f9eede18
Ole Lynge Soerensen <olelynge@gmail.com>
2011-08-11 10:09:02 +0000

    Moving program runner to core.
    git-svn-id: http://localhost:81/svn/dev/modules/Uncas.Core/trunk@1495 90d2b0b2-0e11-ba47-89f1-85e69862abc4

---
+++ src/Uncas.Core/Services/ProgramRunner.cs
@@ -0,0 +1,114 @@
+namespace Uncas.Core.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Reflection;
+    using System.ServiceProcess;
+
+    /// <summary>
+    /// Handles program run.
+    /// </summary>
+    public class ProgramRunner
+    {
+        /// <summary>
+        /// Mapping of console command line args to ServiceManagerCommands.
+        /// </summary>
+        private static readonly Dictionary<string, ServiceManagerCommand> _commands =
+            new Dictionary<string, ServiceManagerCommand>
+                {
+                    { "-console", ServiceManagerCommand.Application },
+                    { "-install", ServiceManagerCommand.Install },
+                    { "-uninstall", ServiceManagerCommand.Uninstall },
+                    { "-start", ServiceManagerCommand.Start },
+                    { "-stop", ServiceManagerCommand.Stop }
+                };
+
+        /// <summary>
+        /// Runs the program.
+        /// </summary>
+        /// <param name="args">The args.</param>
+        /// <param name="actionToRun">The action to run.</param>
+        /// <param name="serviceName">Name of the service.</param>
+        /// <param name="getServiceToRun">The get service to run.</param>
+        public void RunProgram(
+            string[] args,
+            Action actionToRun,
+            string serviceName,
+            Func<ServiceBase> getServiceToRun)
+        {
+            try
+            {
+                if (Debugger.IsAttached)
+                {
+                    Debugger.Break();
+                }
+
+                if (args.Length == 0)
+                {
+                    ServiceBase.Run(getServiceToRun());
+                    return;
+                }
+
+                ServiceManagerCommand command;
+                if (!TryParseCommandLine(args, out command))
+                {
+                    PrintUsage();
+                    return;
+                }
+
+                var serviceManager = new ServiceManager(serviceName);
+                if (command == ServiceManagerCommand.Application)
+                {
+                    const string message = @"Running in console mode.";
+                    Console.WriteLine(message);
+                    actionToRun();
+                    Console.Read();
+                }
+                else
+                {
+                    serviceManager.RunCommand(command);
+                }
+            }
+            catch (Exception ex)
+            {
+                EventLog.WriteEntry(
+                    serviceName,
+                    ex.ToString(),
+                    EventLogEntryType.Error);
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            string exeName = Assembly.GetExecutingAssembly().ManifestModule.Name;
+            Console.WriteLine("Usage:");
+            foreach (var item in _commands)
+            {
+                Console.WriteLine(@"  " + exeName + @" " + item.Key);
+            }
+
+            Console.Read();
+        }
+
+        private static bool TryParseCommandLine(
+            string[] args,
+            out ServiceManagerCommand command)
+        {
+            command = ServiceManagerCommand.Unknown;
+            if (args.Length > 1)
+            {
+                return false;
+            }
+
+            string commandLineArg = args[0];
+            if (_commands.ContainsKey(commandLineArg))
+            {
+                command = _commands[commandLineArg];
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
