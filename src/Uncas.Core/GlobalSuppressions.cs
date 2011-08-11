a8190a049bb5f1b798a55c757f91b17f86eeb659
Ole Lynge Soerensen <olelynge@gmail.com>
2011-08-11 11:24:54 +0000

    Fixing issue.
    git-svn-id: http://localhost:81/svn/dev/modules/Uncas.Core/trunk@1502 90d2b0b2-0e11-ba47-89f1-85e69862abc4

--- src/Uncas.Core/GlobalSuppressions.cs
+++ src/Uncas.Core/GlobalSuppressions.cs
@@ -4,3 +4,4 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Uncas.Core.Logging")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Uncas.Core.Web")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Uncas.Core.Web.WebControls")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Uncas.Core.Services")]
