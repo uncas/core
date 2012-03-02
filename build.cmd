e5b69ca6cddceab0b05554ca20c5b52571c43e1b
Ole Lynge Soerensen <olelynge@gmail.com>
2012-03-02 19:21:59 +0100

    Adds build script and versioning according to git revision.

---
+++ build.cmd
@@ -0,0 +1,1 @@
+"c:\program files (x86)\nant\NAnt.exe" -buildfile:core.build %*
--- src/Uncas.Core/Properties/AssemblyInfo.cs
+++ src/Uncas.Core/Properties/AssemblyInfo.cs
@@ -4,19 +4,14 @@ using System.Resources;
 using System.Runtime.InteropServices;
 using System.Web.UI;
 
-[assembly: AssemblyCompany("Uncas")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCopyright("Copyright © Ole Lynge Sørensen 2012")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyDescription("Infrastructure that does *not* depend on external libraries")]
-[assembly: AssemblyProduct("Uncas.Core")]
 [assembly: AssemblyTitle("Uncas.Core")]
 [assembly: AssemblyTrademark("")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("7b79d9ad-d4fe-4a54-a3e4-85a6c0c8d3a7")]
 [assembly: NeutralResourcesLanguage("en-US")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: WebResource("Uncas.Core.Web.WebControls.FlashPlayer.swf", "application/x-shockwave-flash")]
 [assembly: WebResource("Uncas.Core.Web.WebControls.swfobject.js", "application/x-javascript")]
